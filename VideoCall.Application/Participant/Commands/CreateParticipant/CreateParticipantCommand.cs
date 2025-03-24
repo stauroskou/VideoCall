9bbf5ac324a2d10192acbe63ae1fdd499de1026c
Stavros Koutsoukos <stauroskoutsoukos@gmail.com>
2025-03-24 18:20:01 +0200

    fix(VideoCall): started creating commands and queries for the mediatr

---
+++ VideoCall.Application/Participant/Commands/CreateParticipant/CreateParticipantCommand.cs
@@ -0,0 +1,5 @@
+﻿using CSharpApp.Application.Abstractions.Messaging;
+
+namespace VideoCall.Application.Participant.Commands.CreateParticipant;
+
+public record CreateParticipantCommand(string Name) : ICommand<Guid>;
---
+++ VideoCall.Application/Participant/Commands/CreateParticipant/CreateParticipantCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using CSharpApp.Application.Abstractions.Messaging;
+using VideoCall.Core.Shared;
+
+namespace VideoCall.Application.Participant.Commands.CreateParticipant;
+
+internal sealed class CreateParticipantCommandHandler : ICommandHandler<CreateParticipantCommand, Guid>
+{
+    public Task<Result<Guid>> Handle(CreateParticipantCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
