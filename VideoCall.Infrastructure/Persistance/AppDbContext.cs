6a23cbccecef2028a6c9ccea24a73f0f726eb206
Stavros Koutsoukos <stauroskoutsoukos@gmail.com>
2025-03-24 18:19:20 +0200

    fix(VideoCall): added app db context for EF core

--- VideoCall.Infrastructure/Persistance/AppDbContext.cs
+++ VideoCall.Infrastructure/Persistance/AppDbContext.cs
@@ -3,20 +3,8 @@ using VideoCall.Core.Entities;
 
 namespace VideoCall.Infrastructure.Persistance;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
-
     public DbSet<Session> Sessions { get; set; }
     public DbSet<Participant> Participants { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Session>()
-            .HasMany(s => s.Participants)
-            .WithOne(p => p.Session)
-            .HasForeignKey(p => p.SessionId);
-
-        base.OnModelCreating(modelBuilder);
-    }
 }
