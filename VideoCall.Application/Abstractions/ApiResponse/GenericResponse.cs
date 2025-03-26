2a6fe5b1cb6c5c7d547b5409db1dc808c8e93900
Stavros Koutsoukos <stauroskoutsoukos@gmail.com>
2025-03-26 03:13:49 +0200

    fix(videocall): added assembly reference for application and implemented a generic stractured response for the api

---
+++ VideoCall.Application/Abstractions/ApiResponse/GenericResponse.cs
@@ -0,0 +1,24 @@
+﻿using VideoCall.Core.Shared;
+
+namespace VideoCall.Application.Abstractions.ApiResponse;
+
+public class GenericResponse<T>
+{
+    public bool Success { get; set; }
+    public Error? ErrorMessage { get; set; }
+    public T? Data { get; set; }
+
+    public GenericResponse(T data)
+    {
+        Success = true;
+        ErrorMessage = null;
+        Data = data;
+    }
+
+    public GenericResponse(Error error)
+    {
+        Success = false;
+        ErrorMessage = error;
+        Data = default;
+    }
+}
---
+++ VideoCall.Core/Errors/DomainErrors.cs
@@ -0,0 +1,20 @@
+﻿
+using VideoCall.Core.Shared;
+
+namespace VideoCall.Core.Errors;
+
+public static class DomainErrors
+{
+    public static class ParticipantErrors
+    {
+        public static Error ParticipantNotFound => new Error("Participant.NotFound", "Participant not found.");
+        public static Error ParticipantAlreadyExists => new Error("Participant.Exists", "Participant already exists.");
+    }
+
+    public static class SessionErrors
+    {
+        public static Error SessionNotFound => new Error("Session.NotFound", "Session not found.");
+        public static Error SessionAlreadyExists => new Error("Session.Exists", "Session already exists.");
+    }
+
+}
