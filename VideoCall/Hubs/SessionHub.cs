baeb3eadd91d1fd4d9c481e9e3185ce28fd52459
Stavros Koutsoukos <stauroskoutsoukos@gmail.com>
2025-03-28 04:18:36 +0200

    feature(videocall): added signalR for real time

---
+++ VideoCall/Hubs/SessionHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace VideoCall.Api.Hubs;
+
+
+public class SessionHub() : Hub
+{
+    public async Task NotifyParticipantJoinned(string sessionId)
+    {
+        await Clients.All.SendAsync("ParticipantJoinned", $"{Context.ConnectionId} has joined the session {sessionId}.");
+    }
+
+    public async Task NotifyParticipantLeft(string sessionId)
+    {
+        await Clients.All.SendAsync("ParticipantLeft", $"{Context.ConnectionId} has left the session {sessionId}.");
+    }
+
+    public async Task NotifySessionCreated(string userId)
+    {
+        await Clients.All.SendAsync("SessionCreated", userId);
+    }
+
+    public async Task NotifySessionDeleted(string sessionId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, sessionId);
+        await Clients.Group(sessionId).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has left the group {sessionId}.");
+    }
+
+    public async Task NotifySessionUpdated(string sessionId, string message)
+    {
+        await Clients.Group(sessionId).SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+    }
+}
---
+++ VideoCall/Notifications/SessionNotifier.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using VideoCall.Api.Hubs;
+using VideoCall.Core.Dto;
+using VideoCall.Core.Entities;
+using VideoCall.Core.Interfaces;
+
+namespace VideoCall.Api.Notifications;
+
+public class SessionNotifier(IHubContext<SessionHub> hub) : ISessionNotifier
+{
+    public Task NotifyParticipantAdded(string userId, string sessionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyParticipantLeft(string userId, string sessionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task NotifySessionCreated(SessionDto session)
+    {
+        await hub.Clients.All.SendAsync("SessionCreated", session);
+    }
+
+    public Task NotifySessionDeleted(string userId)
+    {
+        throw new NotImplementedException();
+    }
+}
